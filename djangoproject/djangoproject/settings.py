c66b0958c5c9b2d5784e7ce3e38a914428428c8f
skochv04 <skochevenko@student.agh.edu.pl>
2024-05-17 14:00:26 +0200

    Connect to database

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'CbKeFOQOOVyczYOXPHBpljqfKFJNtbdl',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': '44912',
     }
 }
 
