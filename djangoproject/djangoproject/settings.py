8762cdc2b570d7719158eb5d45f4f342ccbdfe24
skochv04 <skochevenko@student.agh.edu.pl>
2024-05-18 18:47:49 +0200

    Correct

--- djangoproject/djangoproject/settings.py
+++ djangoproject/djangoproject/settings.py
@@ -99,14 +99,16 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
-        # 'ENGINE': 'django.db.backends.postgresql',
-        # 'NAME': 'railway',
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'CbKeFOQOOVyczYOXPHBpljqfKFJNtbdl',
-        # 'HOST': 'monorail.proxy.rlwy.net',
-        # 'PORT': '44912',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR , 'db.sqlite3'),
+
+        
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'qeVVEvyMMRDYMrxbjebxyMusSnPXRqFd',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '36427',
     }
 }
 
--- djangoproject/user_api/models.py
+++ djangoproject/user_api/models.py
@@ -19,7 +19,7 @@ class AppUserManager(BaseUserManager):
 		if not password:
 			raise ValueError('A password is required.')
 		user = self.create_user(email, password)
-		user.is_superuser = True
+		# user.is_superuser = True
 		user.save()
 		return user
 
@@ -28,10 +28,17 @@ class AppUser(AbstractBaseUser, PermissionsMixin):
     user_id = models.AutoField(primary_key=True)
     email = models.EmailField(max_length=50, unique=True)
     username = models.CharField(max_length=50)
-    is_superuser = models.BooleanField(default=False)  # Додано поле is_superuser
+
+    first_name = None
+    last_name = None
+
+    # is_superuser = models.BooleanField(default=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
     objects = AppUserManager()
+
+    # class Meta:
+    #     db_table = 'auth_user'
 
     def __str__(self):
         return self.username
